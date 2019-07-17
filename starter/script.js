faa30d93afc8481d86b87c80b6d66bb8f3079ddb
ANS NEOphyte <jpbrown1023@gmail.com>
2019-07-16 23:09:08 -0400

    Object.create vs Constructors

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 // john Object
 var john = {
     name: 'John',
@@ -37,7 +37,24 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
 
+// Object.create
 
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
 
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
+var jane = Object.create(personProto, 
+{
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer'}
+})
