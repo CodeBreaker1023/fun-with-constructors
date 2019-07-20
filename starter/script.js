d94f65b89e653f8b52941cd1ba7e17097c2a8a45
ANS NEOphyte <jpbrown1023@gmail.com>
2019-07-19 23:42:24 -0400

    prep for next lecture

--- starter/script.js
+++ starter/script.js
@@ -227,57 +227,60 @@ interviewQuestion('teacher')('John');
 
 // Bind, Call, and Apply
 
-var john = {
-    name: 'John',
-    age: 26,
-    job: 'teacher', 
-    presentation: function(style, timeOfDay) {
-        if (style === 'formal') {
-            console.log('Good ' + timeOfDay + ', ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ', and I\'m ' + this.age + ' years old.');
-        } else if (style === 'friendly') {
-            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ', and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
-        } 
-    }
-}
+// var john = {
+//     name: 'John',
+//     age: 26,
+//     job: 'teacher', 
+//     presentation: function(style, timeOfDay) {
+//         if (style === 'formal') {
+//             console.log('Good ' + timeOfDay + ', ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ', and I\'m ' + this.age + ' years old.');
+//         } else if (style === 'friendly') {
+//             console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ', and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+//         } 
+//     }
+// }
 
-var emily = {
-    name: 'Emily',
-    age: 35,
-    job: 'designer'
-};
+// var emily = {
+//     name: 'Emily',
+//     age: 35,
+//     job: 'designer'
+// };
 
-john.presentation('formal', 'morning');
-// Method borrowing = using the john method and calling on emily onject
-john.presentation.call(emily, 'friendly', 'afternoon');
+// john.presentation('formal', 'morning');
+// // Method borrowing = using the john method and calling on emily onject
+// john.presentation.call(emily, 'friendly', 'afternoon');
 
-// john.presentation.apply(emily, 'friendly', 'afternoon');
+// // john.presentation.apply(emily, 'friendly', 'afternoon');
 
-var johnFriendly = john.presentation.bind(john, 'friendly');
+// var johnFriendly = john.presentation.bind(john, 'friendly');
 
-//carrying = creating a function based on another function with some preset parameters 
-johnFriendly('morning');
-johnFriendly('night');
+// //carrying = creating a function based on another function with some preset parameters 
+// johnFriendly('morning');
+// johnFriendly('night');
 
-var emilyFormal = john.presentation.bind(emily,'formal');
-emilyFormal('afternoon');
+// var emilyFormal = john.presentation.bind(emily,'formal');
+// emilyFormal('afternoon');
 
-var years = [1990, 1965, 1937, 2005, 1998];
+// var years = [1990, 1965, 1937, 2005, 1998];
 
-function arrayCalc(arr, fn) {
-    var arrRes = [];
-    for (var i = 0; i < arr.length; i++) {
-        //Remember push adds data to the end of an array
-        arrRes.push(fn(arr[i]));
-    }
-    return arrRes;
-}
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         //Remember push adds data to the end of an array
+//         arrRes.push(fn(arr[i]));
+//     }
+//     return arrRes;
+// }
 
-function calculateAge(el) {
-    return 2016 - el;
-}
+// function calculateAge(el) {
+//     return 2016 - el;
+// }
 
-function isFullAge(el) {
-    return el >= limit;
-}
+// function isFullAge(limit, el) {
+//     return el >= limit;
+// }
 
-var ages = arrayCalc(years,calculateAge);
+// var ages = arrayCalc(years,calculateAge);
+// var fullJapan = arrayCalc(ages,isFullAge.bind(this,20));
+// console.log(fullJapan);
+// console.log(ages);
